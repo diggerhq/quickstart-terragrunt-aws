a27427e407a30d719393cf4e40bd693bc35256e5
Mohamed Habib <moe.habib9@gmail.com>
2024-05-30 12:08:26 +0100

    Update main.tf

--- projects/test1/terraform/main.tf
+++ projects/test1/terraform/main.tf
@@ -1,9 +1,8 @@
 variable "environment" {
-
 }
 
 resource "aws_s3_bucket" "example" {
-  bucket_prefix = "my-tf-test-bucket"
+  bucket_prefix = "my-tf-test-bucket2"
 
   tags = {
     Name        = "My bucket ${var.environment}"
